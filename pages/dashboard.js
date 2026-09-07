01cb1233812d318838564df5c694ce49d7b13989
assemalryan29-svg <assemalryan29@gmail.com>
2026-09-07 20:37:38 +0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from './lib/supabaseClient';
 
 export default function Dashboard() {
   const [loading, setLoading] = useState(true);
