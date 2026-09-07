af8217bbb8784a7ac4e9eb806b2da32476ce5284
assemalryan29-svg <assemalryan29@gmail.com>
2026-09-07 19:20:59 +0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,5 @@
 import { useEffect, useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://ilcdruyuhivhhsrkqjje.supabase.co';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsY2RydXl1aGl2aGhzcmtxamplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NDIxODUsImV4cCI6MjA3NTQxODE4NX0.YOUR_ANON_KEY_HERE';
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+import { supabase } from '../lib/supabaseClient';
 
 export default function Dashboard() {
   const [loading, setLoading] = useState(true);
