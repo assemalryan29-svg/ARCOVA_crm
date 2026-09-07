4f52c2166a83b775935346bbc061a9058981c6f3
assemalryan29-svg <assemalryan29@gmail.com>
2026-09-07 18:58:00 +0300

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from './lib/supabaseClient';
 
 export default function Dashboard() {
   const router = useRouter();
