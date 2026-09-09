9fba1884110cf0ae654c1290e3ee3a1692a71cad
assemalryan29-svg <assemalryan29@gmail.com>
2026-09-09 14:00:02 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ilcdruyuhivhhsrkqjje.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlsY2RydXl1aGl2aGhzcmtxamplIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzOTE2NzQsImV4cCI6MjEwMzk2NzY3NH0.UhFKSRq4LJ-2Fsgb0MMe3I6S60WPFEm0k4zFS4kncvI';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'YOUR_SUPABASE_URL';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'YOUR_SUPABASE_ANON_KEY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
