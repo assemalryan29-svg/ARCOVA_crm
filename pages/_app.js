2369c3363039ed6ca16d4f2dae17bca01dd4dde3
Assem Al Ryan <assemalryan29@gmail.com>
2026-09-20 06:11:13 +0300

    Register ARCOVA PWA service worker

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+
+export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (!('serviceWorker' in navigator)) return;
+
+    navigator.serviceWorker.register('/sw.js').catch((error) => {
+      console.error('ARCOVA service worker registration failed:', error);
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
+}
