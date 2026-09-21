f1d761023cd4b0ba23ba92ad11ebd650548dc909
Assem Al Ryan <assemalryan29@gmail.com>
2026-09-21 18:00:53 +0300

    feat: wire global language provider into all pages

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,12 @@ import { useEffect } from 'react';
 import '../styles/arcova-theme.css';
 import '../styles/arcova-dashboard-theme.css';
 import '../styles/arcova-overrides.css';
+import { LanguageProvider } from '../lib/language';
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
     if (!('serviceWorker' in navigator)) return;
     navigator.serviceWorker.register('/sw.js').catch((error) => console.error('ARCOVA service worker registration failed:', error));
   }, []);
-  return <Component {...pageProps} />;
+  return <LanguageProvider><Component {...pageProps} /></LanguageProvider>;
 }
